6c070d910c322176a8115ac5a9b39236b0da7982
Chirag <Ichiragtaluja@gmail.com>
2023-09-12 19:05:25 -0400

    feat: Part 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Part 1
 const hospital = {
   name: "Oakville Trafalgar Memorial Hospital",
-  patient: [
+  patients: [
     {
       id: 1,
       fullName: "Chai Ray",
@@ -18,7 +18,26 @@ const hospital = {
       id: 3,
       fullName: "Alex Styles",
       dateOfBirth: new Date(2006, 4, 2),
-      symptoms: ["congestion", "muscle ache", "tiredness"],
+      symptoms: ["congestion", "muscle-ache", "tiredness"],
     },
   ],
 };
+
+//Part2
+function showPatients(hospital) {
+  const name = hospital.name;
+  const patients = hospital.patients;
+  let finalOutput = `<h1>${name}</h1> \n`;
+  for (let i = 0; i < patients.length; i++) {
+    let patient = patients[i];
+    let patientDetails = `<h2>${patient.fullName}, ${patient.dateOfBirth} </h2> \n <ul> \n`;
+
+    for (let j = 0; j < patient.symptoms.length; j++) {
+      patientDetails += `<li>${patient.symptoms[j]}</li> \n`;
+    }
+
+    finalOutput += patientDetails + "</ul> \n";
+  }
+}
+
+showPatients(hospital);
