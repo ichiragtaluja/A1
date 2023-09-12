39ecb4c542623e90302f96445d0f51a293a3514a
Chirag <Ichiragtaluja@gmail.com>
2023-09-12 16:13:47 -0400

    feat: Part 1

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert("I'm here");
+//Part 1
+const hospital = {
+  name: "Oakville Trafalgar Memorial Hospital",
+  patient: [
+    {
+      id: 1,
+      fullName: "Chai Ray",
+      dateOfBirth: new Date(1992, 2, 4),
+      symptoms: ["fever", "diarrhea"],
+    },
+    {
+      id: 2,
+      fullName: "Chirag Taluja",
+      dateOfBirth: new Date(2005, 9, 14),
+      symptoms: ["fatigue", "coughing"],
+    },
+    {
+      id: 3,
+      fullName: "Alex Styles",
+      dateOfBirth: new Date(2006, 4, 2),
+      symptoms: ["congestion", "muscle ache", "tiredness"],
+    },
+  ],
+};
