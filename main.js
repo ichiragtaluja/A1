ff14c08364518e3fced9903b7718324788b798e0
Chirag <Ichiragtaluja@gmail.com>
2023-09-18 13:15:32 -0400

    refactor Part 2 and complete part3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Part 1
 const hospital = {
   name: "Oakville Trafalgar Memorial Hospital",
-  patients: [
+  patient: [
     {
       id: 1,
       fullName: "Chai Ray",
@@ -26,18 +26,45 @@ const hospital = {
 //Part2
 function showPatients(hospital) {
   const name = hospital.name;
-  const patients = hospital.patients;
-  let finalOutput = `<h1>${name}</h1> \n`;
+  const patients = hospital.patient;
+
+  let finalOutput = "<h1>" + name + "</h1> \n";
+
   for (let i = 0; i < patients.length; i++) {
     let patient = patients[i];
-    let patientDetails = `<h2>${patient.fullName}, ${patient.dateOfBirth} </h2> \n <ul> \n`;
+
+    //date of birth without time
+    let dateOfBirthWithoutTime =
+      patient.dateOfBirth.getDate() +
+      " " +
+      patient.dateOfBirth.getMonth() +
+      " " +
+      patient.dateOfBirth.getFullYear();
+
+    let patientDetails =
+      "<h2>" +
+      patient.fullName +
+      "," +
+      dateOfBirthWithoutTime +
+      " " +
+      "</h2> \n <ul> \n";
 
     for (let j = 0; j < patient.symptoms.length; j++) {
-      patientDetails += `<li>${patient.symptoms[j]}</li> \n`;
+      patientDetails += "\t<li>" + patient.symptoms[j] + "</li> \n";
     }
 
     finalOutput += patientDetails + "</ul> \n";
   }
+  return finalOutput;
 }
 
-showPatients(hospital);
+console.log(showPatients(hospital));
+
+//Part3
+function getPatient(patients) {
+  //random number less than or equal to length of array of patients
+  const randomNumber = Math.floor(Math.random() * patients.length);
+  return patients[randomNumber].id;
+}
+
+console.log(getPatient(hospital.patient));
